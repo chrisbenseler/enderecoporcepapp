ab07a6cd5c292dae4100f55619d7b8c1586f51e8
Christian <cbenseler@gmail.com>
2017-02-24 10:42:43 -0300

    chore: build script

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+ionic build android --release
+
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ceppromiseapp.keystore platforms/android/build/outputs/apk/android-release-unsigned.apk ceppromiseapp
 
 zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk  platforms/android/build/outputs/apk/ceppromiseapp.apk
